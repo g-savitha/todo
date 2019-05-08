45edf3a38e20edfd25f489d4cd89df4c0e1354c9
Gollamudi Savitha Rakendu Sri <31612459+savitha-gollamudi@users.noreply.github.com>
2019-05-08 23:47:50 +0530

    Update db.js
    used environment variable

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports ={
-     mongoURI: 'mongodb+srv://savitha:savitha@todoauth-0nd43.mongodb.net/test?retryWrites=true'
+     //mongoURI: 'mongodb+srv://savitha:savitha@todoauth-0nd43.mongodb.net/test?retryWrites=true'
+     mongoURI:process.env.MONGO_URI
 }
 
