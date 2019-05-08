8f742f4cd831d117eec05965b35374feb7efb508
Gollamudi Savitha Rakendu Sri <31612459+savitha-gollamudi@users.noreply.github.com>
2019-05-08 22:38:22 +0530

    Update db.js
    Deploying only to mlab, to avoid network issues

--- config/db.js
+++ config/db.js
@@ -1,10 +1,2 @@
-if(process.env.NODE_ENV==='production'){
-    module.exports = {
-        mongoURI: 'mongodb+srv://savitha:savitha@todoauth-8orh1.mongodb.net/test?retryWrites=true'
-    }
-}
-else{
-    module.exports = {
-        mongoURI: 'mongodb://localhost/todo'
-    }
-}
+mongoURI: 'mongodb+srv://savitha:savitha@todoauth-8orh1.mongodb.net/test?retryWrites=true'
+
