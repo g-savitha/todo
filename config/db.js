da9b50f702dbf2ca6c49891e5279558a9f4747a1
Gollamudi Savitha Rakendu Sri <31612459+savitha-gollamudi@users.noreply.github.com>
2019-05-08 23:43:27 +0530

    Update db.js
    reverted back the changes

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports ={
-  mongoURI: 'mongodb+srv://savitha:savitha@todoauth-shard-00-00-0nd43.mongodb.net:27017,todoauth-shard-00-01-0nd43.mongodb.net:27017,todoauth-shard-00-02-0nd43.mongodb.net:27017/test?ssl=true&replicaSet=todoauth-shard-0&authSource=admin&retryWrites=true'
+     mongoURI: 'mongodb+srv://savitha:savitha@todoauth-0nd43.mongodb.net/test?retryWrites=true'
 }
 
